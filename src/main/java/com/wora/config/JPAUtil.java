1008c9589832d9ad6538f58a5520f85c9bc2468f
hamzalamin <hamzalamin80@gmail.com>
2024-10-01 12:12:05 +0200

    Add persistence.xml configuration for JPA with PostgreSQL

---
+++ src/main/java/com/wora/config/JPAUtil.java
@@ -0,0 +1,24 @@
+package com.wora.config;
+
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JPAUtil {
+    private static EntityManagerFactory ENTITY_MANAGER_FACTORY;
+    private JPAUtil(){}
+
+    public static EntityManagerFactory entityManagerFactory(){
+        if (ENTITY_MANAGER_FACTORY == null){
+            ENTITY_MANAGER_FACTORY = Persistence.createEntityManagerFactory("simulation");
+        }
+        return ENTITY_MANAGER_FACTORY;
+    }
+
+    public static void closeEntityManagerFactory() {
+        if (ENTITY_MANAGER_FACTORY != null) {
+            ENTITY_MANAGER_FACTORY.close();
+        }
+    }
+
+}
