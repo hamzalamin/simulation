c4c6c4e326b79676f67aaf8451dfdad340dd66fe
hamzalamin <hamzalamin80@gmail.com>
2024-10-08 01:37:24 +0200

    Implement StatusController with initialization of statusRepository and statusService

--- src/main/java/com/wora/smartbank2/web/controllers/StatusController.java
+++ src/main/java/com/wora/smartbank2/web/controllers/StatusController.java
@@ -1,4 +1,26 @@
 package com.wora.smartbank2.web.controllers;
 
-public class StatusController {
+import com.wora.smartbank2.repositories.IStatusRepository;
+import com.wora.smartbank2.repositories.impl.RequestRepository;
+import com.wora.smartbank2.repositories.impl.StatusRepository;
+import com.wora.smartbank2.services.IStatusService;
+import com.wora.smartbank2.services.impl.RequestService;
+import com.wora.smartbank2.services.impl.StatusService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.validation.Validation;
+
+@WebServlet("/status/*")
+public class StatusController extends HttpServlet {
+    private IStatusRepository statusRepository;
+    private IStatusService statusService;
+
+    @Override
+    public void init() throws ServletException {
+        this.statusRepository = new StatusRepository();
+        this.statusService = new StatusService(statusRepository, Validation.buildDefaultValidatorFactory().getValidator());
+    }
+
+
 }
